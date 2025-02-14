305b7c8fad2f77ffee62a9ce47f2ba8659903c27
Basmala Walied <bosywaleed192@gmail.com>
2025-02-14 21:03:42 +0200

    Update Interface.h

--- Interface.h
+++ Interface.h
@@ -15,18 +15,17 @@ class DataSourceInterface
 {
 public:
     virtual ~DataSourceInterface() = default;
-
-    // virtual void addClient(Client) = 0;
+     virtual void addClient(Client) = 0;
     // virtual void addEmployee(Employee) = 0;
     virtual void addAdmin(Admin) = 0;
 
-    // virtual void getAllClients() = 0;
+    virtual void getAllClients() = 0;
     // virtual void getAllEmployees() = 0;
     virtual void getAllAdmins() = 0;
 
     virtual bool adminExists() = 0;
 
-    // virtual void removeAllClients() = 0;
+    virtual void removeAllClients() = 0;
     // virtual void removeAllEmployees() = 0;
     virtual void removeAllAdmins() = 0;
 };
