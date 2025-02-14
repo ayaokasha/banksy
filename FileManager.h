0fbba3c8446812b1846e0550cba98c1ff1a5c526
Basmala Walied <bosywaleed192@gmail.com>
2025-02-14 21:11:59 +0200

    Update FileManager.h

--- FileManager.h
+++ FileManager.h
@@ -7,12 +7,53 @@
 #include "FilesHelper.h"
 #include "Interface.h"
 #include "Admin.h"
-
+# include "Client"
 using namespace std;
 
 class FileManager : public DataSourceInterface
 {
 public:
+     bool File() override {
+        ifstream ClientFile("Client.txt");
+        if(!ClientFile){
+            cout << "File not found" << endl;
+            return false
+        }
+            string line;
+            if(getline(ClientFile , line)){
+                ClientFile.close();
+                return true;
+            }
+            ClientFile.close();
+            return false;
+    }
+    void addClient(Client client) override {
+        ofstream ClientFile("Client.txt",ios::app);
+        if(!ClientFile){
+            cout << "Unable to open file" << endl;
+            return;
+        }
+        ClientFile << client.getId() << " " << client.getName() << " " << client.getPassword << " " << client.balance() << endl;
+        ClientFile.close();
+        cout << "Client added successfully" << endl;
+    }
+    void getAllClient() override {
+        ifstream ClientFile("Client.txt");
+        if(!ClientFile){
+            cout << "UNable to open file" << endl;
+        }
+        string line;
+        while (getline(ClientFile , line))
+        {
+            cout << line << endl;
+        }
+         ClientFile.close();
+    }
+     void removeAllClient() override {
+        ofstream ClientFile("Client.txt", ios::trunc);
+        ClientFile.close();
+        cout << "All client have been remove successfully" << endl;
+    }
     bool adminExists() override
     {
         ifstream file("admin.txt");
@@ -57,5 +98,6 @@ public:
         ofstream file("admin.txt", ios::trunc);
         file.close();
     }
+
 };
 #endif
