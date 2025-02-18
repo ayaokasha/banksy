2193fd06a8b2b75d23544d77af74ce013cc94beb
nada ashraf <na5385364@gmail.com>
2025-02-18 19:29:33 +0200

    Update FilesHelper.h

--- FilesHelper.h
+++ FilesHelper.h
@@ -72,6 +72,18 @@ public:
         fm.getAllEmployees();
     }
 
+
+
+
+  static void ClearFile(string filename, string lastidfile)
+    {
+        ofstream Employeefile(filename, ios::trunc);
+        Employeefile.close();
+        savelastEmployee(lastidfile, 0);
+    }
+};
+
+
     // admin
     static void savelastAdmin(string filename, int id)
     {
@@ -99,5 +111,6 @@ public:
         savelastAdmin(lastidfile, 0);
     }
 };
+
 
 #endif
