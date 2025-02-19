cbd09d9049d31a7acd09121631bb9309c12242bf
Basmala Walied <bosywaleed192@gmail.com>
2025-02-19 02:39:25 +0200

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -63,10 +63,10 @@ public:
         }
     }
     void listClients(){
-        if(clientList.empty()){
+        if(clients.empty()){
             cout << "No clients available" << endl;
         }
-        for(auto &client : clientList)
+        for(auto &client : clients)
             client.display();
     }
         
